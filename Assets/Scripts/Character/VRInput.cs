1970748089e46612fff2833ab6843d5fa36aa9ca
Fabien Marchand <ghost.fm65@gmail.com>
2024-03-04 17:03:51 +0100

    adding simulator debug mode

---
+++ Assets/Scripts/Character/VRInput.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VRInput : MonoBehaviour
+{
+    public InputActionProperty inputAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue  = inputAction.action.ReadValue<float>();
+
+        Debug.Log(triggerValue);
+    }
+}
