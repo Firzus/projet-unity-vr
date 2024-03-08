c1b2664afc5eb9fbd35d8cbd70a2adcbdffdcb24
Firzus <lprieu@gaming.tech>
2024-03-08 11:54:59 +0100

    Update XRSocketTagInteractor.cs

--- Assets/Scripts/XR/XRSocketTagInteractor.cs
+++ Assets/Scripts/XR/XRSocketTagInteractor.cs
@@ -3,15 +3,15 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRSocketTagInteractor : XRSocketInteractor
 {
-    [SerializeField] private string targetTag;
+    [SerializeField] private string _targetTag;
 
     public override bool CanHover(IXRHoverInteractable interactable)
     {
-        return base.CanHover(interactable) && interactable.transform.tag == targetTag;
+        return base.CanHover(interactable) && interactable.transform.tag == _targetTag;
     }
 
     public override bool CanSelect(IXRSelectInteractable interactable)
     {
-        return base.CanSelect(interactable) && interactable.transform.tag == targetTag;
+        return base.CanSelect(interactable) && interactable.transform.tag == _targetTag;
     }
 }
