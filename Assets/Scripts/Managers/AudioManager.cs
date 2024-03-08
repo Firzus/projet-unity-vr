dd84d47e34cd4ae700a76ff70f76188a22a38da8
Firzus <lprieu@gaming.tech>
2024-03-08 01:53:58 +0100

    update

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -21,11 +21,6 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    private void Start()
-    {
-        PlayMusic("MenuTheme");
-    }
-
     public void PlayMusic(string name)
     {
         Sound s = Array.Find(musicSounds, x => x.name == name);
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,26 +1,52 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float _mTimeLeft;
-
+    public static GameManager Instance { get; private set; }
     private float _mTimer;
 
-    public static GameManager Instance; 
+    // UI
+    [SerializeField] private GameObject _titlePrefab;
 
-    void Start()
+    private void Awake()
     {
         DontDestroyOnLoad(this);
 
         if (Instance == null)
+        {
+
             Instance = this;
+        }
         else if (Instance != this)
+        {
             Destroy(gameObject);
+        }
+    }
+
+    IEnumerator MusicIntro()
+    {
+        yield return new WaitForSeconds(5);
+        AudioManager.Instance.PlayMusic("MenuTheme");
+    }
+
+    IEnumerator TitleIntro()
+    {
+        yield return new WaitForSeconds(7);
+        // Create title
+        Instantiate(_titlePrefab, new Vector3(0, 6, 30), Quaternion.identity);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(MusicIntro());
+        StartCoroutine(TitleIntro());
     }
 
     public void Quit()
     {
-        Quit();
+        Application.Quit();
     }
 
     public float GetTime()
