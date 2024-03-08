fdca4f40ae987ac8e65a4079154ee3126b0f1534
joachim-gabin <ejoachimgabin@gaming.tech>
2024-03-08 13:32:48 +0100

    add more interactive meuble
    add more interactive meuble

--- Assets/Scripts/UI/OutlineInteractable.cs
+++ Assets/Scripts/UI/OutlineInteractable.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class OutlineInteractable : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class OutlineInteractable : MonoBehaviour
 
     void Outlining()
     {
-        if (_mHighlight != null) 
+        if (_mHighlight != null)
         {
             _mHighlight.gameObject.GetComponent<Outline>().enabled = false;
             _mHighlight = null;
@@ -25,9 +26,9 @@ public class OutlineInteractable : MonoBehaviour
         if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out _mHit))
         {
             _mHighlight = _mHit.transform;
-            if (_mHighlight.gameObject.layer == _mTargetLayer) 
+            if (_mHighlight.gameObject.layer == _mTargetLayer && _mHighlight.gameObject.GetComponent<XRGrabInteractable>().interactionLayers == 5)
             {
-                if (_mHighlight.gameObject.GetComponent<Outline>() != null) 
+                if (_mHighlight.gameObject.GetComponent<Outline>() != null)
                 {
                     _mHighlight.gameObject.GetComponent<Outline>().enabled = true;
                 }
@@ -40,6 +41,38 @@ public class OutlineInteractable : MonoBehaviour
             else
             {
                 _mHighlight = null;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == _mTargetLayer)
+        {
+            if (other.gameObject.GetComponent<Outline>() != null)
+            {
+                other.gameObject.GetComponent<Outline>().enabled = true;
+            }
+            else
+            {
+                Outline outline = other.gameObject.AddComponent<Outline>();
+                outline.enabled = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == _mTargetLayer)
+        {
+            if (other.gameObject.GetComponent<Outline>() != null)
+            {
+                other.gameObject.GetComponent<Outline>().enabled = false;
+            }
+            else
+            {
+                Outline outline = other.gameObject.AddComponent<Outline>();
+                outline.enabled = true;
             }
         }
     }
