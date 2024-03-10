ba426387006dff581f9704d99988a07c848ef293
Fabien <84633381+Zearcus@users.noreply.github.com>
2024-03-10 18:53:57 +0100

    nommages

--- Assets/Scripts/UI/Countdown.cs
+++ Assets/Scripts/UI/Countdown.cs
@@ -4,42 +4,42 @@ using System.Collections;
 
 public class Countdown : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro timerText;
-    [SerializeField] private AudioSource beepSound;
-    [SerializeField] private float remainingTimeInSeconds = 0.0f;
-    [SerializeField] private int remainingTimeInMinutes = 0;
-    [SerializeField] private int enterStressModeInSeconds = 0, enterStressModeInMinutes = 0;
+    [SerializeField] private TextMeshPro _timerText;
+    [SerializeField] private AudioSource _beepSound;
+    [SerializeField] private float _remainingTimeInSeconds = 0.0f;
+    [SerializeField] private int _remainingTimeInMinutes = 0;
+    [SerializeField] private int _enterStressModeInSeconds = 0, _enterStressModeInMinutes = 0;
 
-    private IEnumerator sfx;
-    private int minutes = 0, seconds = 0, timer = 0;
-    private bool stressed = false, isPlayingSFXSound = false;
-    public bool awake = false;
+    private IEnumerator _sfx;
+    private int _minutes = 0, _seconds = 0, _timer = 0;
+    private bool _stressed = false, _isPlayingSFXSound = false;
+    public bool Awake = false;
 
     void Start()
     {
-        if (remainingTimeInMinutes > 0)
-            remainingTimeInSeconds = remainingTimeInMinutes * 60;
-        beepSound = transform.GetChild(2).GetComponent<AudioSource>();
+        if (_remainingTimeInMinutes > 0)
+            _remainingTimeInSeconds = _remainingTimeInMinutes * 60;
+        _beepSound = transform.GetChild(2).GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!awake)
+        if (!Awake)
         {
             return;
         }
-        sfx = SfxSound(1.0f);
-        if (!stressed)
+        _sfx = SfxSound(1.0f);
+        if (!_stressed)
         {
             CountdownMethod();
-            if (timer == 0 && remainingTimeInSeconds != 0 && !isPlayingSFXSound)// play bip sound to 60 seconds
+            if (_timer == 0 && _remainingTimeInSeconds != 0 && !_isPlayingSFXSound)// play bip sound to 60 seconds
             {
-                StartCoroutine(sfx);
+                StartCoroutine(_sfx);
             }
-            if (minutes == enterStressModeInMinutes && seconds == enterStressModeInSeconds)// pass to stress mode
+            if (_minutes == _enterStressModeInMinutes && _seconds == _enterStressModeInSeconds)// pass to stress mode
             {
-                stressed = true;
+                _stressed = true;
             }
         }
         else
@@ -52,38 +52,38 @@ public class Countdown : MonoBehaviour
 
     private void CountdownMethod()
     {
-        if (remainingTimeInSeconds > 0)// Countdown
+        if (_remainingTimeInSeconds > 0)// Countdown
         {
-            remainingTimeInSeconds -= Time.deltaTime;
+            _remainingTimeInSeconds -= Time.deltaTime;
         }
-        else if (remainingTimeInSeconds < 0)//when countdown is under 0 stop the countdown
+        else if (_remainingTimeInSeconds < 0)//when countdown is under 0 stop the countdown
         {
-            remainingTimeInSeconds = 0;
+            _remainingTimeInSeconds = 0;
             Explosion();
         }
-        else if (remainingTimeInSeconds == 0)//return nothing if 0
+        else if (_remainingTimeInSeconds == 0)//return nothing if 0
         {
-            remainingTimeInSeconds = 0;
-            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            _remainingTimeInSeconds = 0;
+            _timerText.text = string.Format("{0:00}:{1:00}", _minutes, _seconds);
             return;
         }
         //convert and show the countdown
-        minutes = Mathf.FloorToInt(remainingTimeInSeconds / 60);
-        seconds = Mathf.FloorToInt(remainingTimeInSeconds % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        _minutes = Mathf.FloorToInt(_remainingTimeInSeconds / 60);
+        _seconds = Mathf.FloorToInt(_remainingTimeInSeconds % 60);
+        _timerText.text = string.Format("{0:00}:{1:00}", _minutes, _seconds);
 
-        timer = seconds;
+        _timer = _seconds;
     }
 
     private void BipSound()
     {
-        beepSound.Play();
+        _beepSound.Play();
     }
     private void StressMode()
     {
-        if (timer != 0 && !isPlayingSFXSound)// bip for each seconds
+        if (_timer != 0 && !_isPlayingSFXSound)// bip for each seconds
         {
-            StartCoroutine(sfx);
+            StartCoroutine(_sfx);
         }
         //beepSound.loop = true;
     }
@@ -98,10 +98,10 @@ public class Countdown : MonoBehaviour
 
     IEnumerator SfxSound(float time)
     {
-        isPlayingSFXSound = true;
+        _isPlayingSFXSound = true;
         yield return new WaitForSeconds(time);
         BipSound();
-        isPlayingSFXSound = default;
-        StopCoroutine(sfx);
+        _isPlayingSFXSound = default;
+        StopCoroutine(_sfx);
     }
 }
