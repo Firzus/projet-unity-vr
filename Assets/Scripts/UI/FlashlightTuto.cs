49ace0e8f0dabdfc76cfc7a485a23623f6ffa3d8
joachim-gabin <ejoachimgabin@gaming.tech>
2024-03-13 13:02:48 +0100

    start cookie and tuto zone
    start cookie and tuto zone

--- Assets/Scripts/UI/FlashlightTuto.cs
+++ Assets/Scripts/UI/FlashlightTuto.cs
@@ -1,14 +1,24 @@
+using TMPro;
 using UnityEngine;
 
 public class FlashlightTuto : MonoBehaviour
 {
     [SerializeField] private GameObject _pileToUse;
+    [SerializeField] private TextMeshPro _text;
 
     void Update()
     {
         if(!_pileToUse)
         {
             Destroy(gameObject);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //_text
         }
     }
 }
