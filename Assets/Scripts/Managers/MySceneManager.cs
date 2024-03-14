07e9d9b42a2095a0aef87a473433c71580bc2b8f
Firzus <lprieu@gaming.tech>
2024-03-14 18:40:38 +0100

    Scene Preload

--- Assets/Scripts/Managers/MySceneManager.cs
+++ Assets/Scripts/Managers/MySceneManager.cs
@@ -1,11 +1,12 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MySceneManager : MonoBehaviour
 {
-    [SerializeField] private string _mSceneName;
-    [SerializeField] private FadeScreen _mFadeScreen;
+    [SerializeField] private FadeScreen _fadeScreen;
+    private AsyncOperation _sceneLoadOperation;
+    private string _sceneName;
 
     public static MySceneManager Instance;
 
@@ -19,16 +20,31 @@ public class MySceneManager : MonoBehaviour
             Destroy(gameObject);
     }
 
-    public void ChangeScene(string SceneName)
+    public void ChangeScene(string sceneName)
     {
-        _mSceneName = SceneName;
-        StartCoroutine(GoToSceneRoutine());
-        SceneManager.LoadScene(_mSceneName, LoadSceneMode.Single);
+        _sceneName = sceneName;
+        StartCoroutine(ChangeSceneRoutine());
     }
 
-    IEnumerator GoToSceneRoutine()
+    IEnumerator ChangeSceneRoutine()
     {
-        _mFadeScreen.FadeOut();
-        yield return new WaitForSeconds(_mFadeScreen.FadeDuration());
+        _fadeScreen.FadeOut();
+        // Précharge la scène en arrière-plan
+        _sceneLoadOperation = SceneManager.LoadSceneAsync(_sceneName, LoadSceneMode.Single);
+        // Empêche la scène de s'afficher immédiatement
+        _sceneLoadOperation.allowSceneActivation = false;
+
+        // Attendre que l'écran soit complètement fondu
+        yield return new WaitForSeconds(_fadeScreen.FadeDuration());
+
+        // Maintenant que l'écran est noir, on peut permettre à la scène de s'activer
+        // (vous pourriez vouloir attendre plus longtemps ou vérifier d'autres conditions avant d'activer la scène)
+        _sceneLoadOperation.allowSceneActivation = true;
+
+        // Attendre un petit peu pour permettre à la scène de commencer le chargement
+        yield return new WaitForSeconds(0.1f);
+
+        // Faire le fondu en entrée une fois que la nouvelle scène commence à s'afficher
+        _fadeScreen.FadeIn();
     }
 }
