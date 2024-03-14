df7226287f8c8cd4ddd699013ed3159330a31bc1
Firzus <lprieu@gaming.tech>
2024-03-14 17:06:48 +0100

    Music Game & Rework Managers

--- Assets/Scripts/XR/ActivateTeleportationRay.cs
+++ Assets/Scripts/XR/ActivateTeleportationRay.cs
@@ -1,18 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class ActivateTeleportationRay : MonoBehaviour
 {
     [SerializeField] private InputActionProperty _leftActivate;
     [SerializeField] private  GameObject _teleportationRay;
-    [SerializeField] private XRRayInteractor _leftRay;
-    [SerializeField] private XRRayInteractor _rightRay;
 
     void Update()
     {
-        bool isLeftRayHovering = _leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftnumber, out bool leftValid);
-
         if (_leftActivate.action.ReadValue<float>() > 0.1f)
         {
             _teleportationRay.SetActive(true);
