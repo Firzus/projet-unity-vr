1d2658cc9202e5d018fb0a2a5090e5f54eec3e0b
Fabien Marchand <ghost.fm65@gmail.com>
2024-03-14 19:29:47 +0100

    optimized Outline

--- Assets/Scripts/UI/OutlineInteractable.cs
+++ Assets/Scripts/UI/OutlineInteractable.cs
@@ -7,6 +7,7 @@ public class OutlineInteractable : MonoBehaviour
     [SerializeField] private Transform _mHighlight;
     private RaycastHit _mHit;
     private int _mTargetLayer = 6;
+    private bool _valid = false, _outlined = false, _onTriggered = false;
 
     void Update()
     {
@@ -15,67 +16,87 @@ public class OutlineInteractable : MonoBehaviour
 
     void Outlining()
     {
-        if (_mHighlight != null) 
+        if (_outlined)
         {
             _mHighlight.gameObject.GetComponent<Outline>().enabled = false;
-            _mHighlight = null;
+            _outlined = false;
         }
-
         Ray ray = new Ray(transform.position, transform.forward);
         if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out _mHit))
         {
             _mHighlight = _mHit.transform;
-            if (_mHighlight != null &&
-                _mHighlight.gameObject.layer == _mTargetLayer && 
-                (_mHighlight.gameObject.GetComponent<XRGrabInteractable>().interactionLayers == InteractionLayerMask.GetMask("ray interaction") ||
-                (_mHighlight.gameObject.GetComponent<XRGrabInteractable>().interactionLayers == InteractionLayerMask.GetMask("ray interaction", "direct interaction"))))
-            {
-                if (_mHighlight.gameObject.GetComponent<Outline>() != null) 
-                {
-                    _mHighlight.gameObject.GetComponent<Outline>().enabled = true;
-                }
-                else
-                {
-                    Outline outline = _mHighlight.gameObject.AddComponent<Outline>();
-                    outline.enabled = true;
-                }
-            }
-            else
-            {
-                _mHighlight = null;
-            }
+
+            CheckOutline();
+            GetOrAddOutline();
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     // if (other.CompareTag("LeftHand")  || other.CompareTag("RightHand") || other.CompareTag("Player"))
+    //     //     return;
+    //     // if (other.gameObject.GetComponent<Outline>() != null)
+    //     // {
+    //     //     other.gameObject.GetComponent<Outline>().enabled = true;
+    //     // }
+    //     // else
+    //     // {
+    //     //     Outline outline = other.gameObject.AddComponent<Outline>();
+    //     //     outline.enabled = true;
+    //     // }
+    //     // _onTriggered = true;
+    // }
+
+    // private void OnTriggerExit(Collider other)
+    // {
+    //     // if (other.CompareTag("LeftHand") || other.CompareTag("RightHand") || other.CompareTag("Player"))
+    //     //     return;
+    //     // if (!_onTriggered)
+    //     //     return;
+    //     // if (other.gameObject.GetComponent<Outline>() != null)
+    //     // {
+    //     //     other.gameObject.GetComponent<Outline>().enabled = false;
+    //     // }
+    //     // _onTriggered = false;
+    // }
+
+    private void GetOrAddOutline()
     {
-        if (_mHighlight != null && other.gameObject.layer == _mTargetLayer && _mHighlight.gameObject.GetComponent<XRGrabInteractable>().interactionLayers == InteractionLayerMask.GetMask("direct interaction"))
+        if (!_valid)
+            return;
+        if (_mHighlight.gameObject.GetComponent<XRGrabInteractable>().interactionLayers == InteractionLayerMask.GetMask("ray interaction", "direct interaction"))
         {
-            if (other.gameObject.GetComponent<Outline>() != null)
+            if (_mHighlight.gameObject.GetComponent<Outline>() != null)
             {
-                other.gameObject.GetComponent<Outline>().enabled = true;
+                _mHighlight.gameObject.GetComponent<Outline>().enabled = true;
             }
             else
             {
-                Outline outline = other.gameObject.AddComponent<Outline>();
+
+                Outline outline = _mHighlight.gameObject.AddComponent<Outline>();
                 outline.enabled = true;
             }
+            _outlined = true;
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void CheckOutline()
     {
-        if (_mHighlight != null && other.gameObject.layer == _mTargetLayer && _mHighlight.gameObject.GetComponent<XRGrabInteractable>().interactionLayers == InteractionLayerMask.GetMask("direct interaction"))
+        if (_mHighlight == null)
         {
-            if (other.gameObject.GetComponent<Outline>() != null)
-            {
-                other.gameObject.GetComponent<Outline>().enabled = false;
-            }
-            else
-            {
-                Outline outline = other.gameObject.AddComponent<Outline>();
-                outline.enabled = false;
-            }
+            _valid = default;
+            return;
         }
+        else if (_mHighlight.gameObject.layer != _mTargetLayer)
+        {
+            _valid = default;
+            return;
+        }
+        else if (_mHighlight.gameObject.GetComponent<XRGrabInteractable>() == null)
+        {
+            _valid = default;
+            return;
+        }
+        _valid = true;
     }
 }
